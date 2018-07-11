6c30b7eb67c36e573d9ab7532d7c721e66cdcfa8
Andriy Polukhin <andriy.polukhin@gmail.com>
2018-07-11 23:24:00 +0300

    calendar

---
+++ Projects/CalenderPractice/src/calenderpractice/CalenderPractice.java
@@ -0,0 +1,27 @@
+ 
+package calenderpractice;
+
+//import java.pk1.pkg2.----.pkn.class|*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+//import java.util.*;
+
+
+
+
+public class CalenderPractice {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 68);
+        System.out.println(cal.getTime());
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+        String formatted = format1.format(cal.getTime());
+        System.out.println(formatted);
+    }
+    
+}
